0c5fd1cb6df175ee1f455e21fc393c5075ae9a6d
skipper <skipper@rimbahouse.com>
2024-08-23 18:01:53 +0700

    bugfix: description

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }) {
         {/*====== Required meta tags ======*/}
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta name="description" content="Adventure, Tours, Travel" />
+        <meta name="description" content="Kampung Tematik, Mulaharja" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
